e5a003fbd53c83cb0a701d3f2f3d81965b38949c
Jacobify <farzadyaghoobi_1375@yahoo.com>
2022-11-12 13:45:23 +0330

    Jacobify - using React.createElement

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  const firstName = "Farzad";
-  const lastName = "Yaghoobi";
-  const age = 25;
-  const Job = "Web Dev";
-  const inputPlaceHolder = "Enter your details"
-  const input = <input placeholder={inputPlaceHolder}/>
-  const myArray = [0,1,2,3]
-  const myObject = {age : 35,name : "Fat Gum"}
-
-  const getFullName = (firstName,lastName) => `${firstName} ${lastName}`;
+  // const firstName = "Farzad";
+  // const lastName = "Yaghoobi";
+  // const age = 25;
+  // const Job = "Web Dev";
+  // const inputPlaceHolder = "Enter your details"
+  // const input = <input placeholder={inputPlaceHolder}/>
+  // const myArray = [0,1,2,3]
+  // const myObject = {age : 35,name : "Fat Gum"}
+  // const getFullName = (firstName,lastName) => `${firstName} ${lastName}`;
   
   return (
-    <div className="App">
-      <h3>Full Name : {getFullName(firstName,lastName)}</h3>
-      <p>Job : {myArray[1]}</p>
-      <p>Age : {myObject.age}</p>
-      {/* <input placeholder={inputPlaceHolder}/> */}
-      {input}
-      {
-        myArray[2] > 0  ? "True" : "False"
-      }
-    </div>
+      
+/*#__PURE__*/React.createElement("div", {
+  className: "main-div"
+}, /*#__PURE__*/React.createElement("h3", null, "Heading"), /*#__PURE__*/React.createElement("p", null, "Para"), /*#__PURE__*/React.createElement("p", null, "Para"), /*#__PURE__*/React.createElement("p", null, "Para"), /*#__PURE__*/React.createElement("p", null, "Para"), /*#__PURE__*/React.createElement("p", null, "Para"), /*#__PURE__*/React.createElement("p", null, "Para"), /*#__PURE__*/React.createElement("p", null, "Para"))
   );
 }
 
