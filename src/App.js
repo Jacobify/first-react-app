89640c14f517bf7ed6d9ede3cba6c904e1e562ee
Jacobify <farzadyaghoobi_1375@yahoo.com>
2022-11-12 10:12:05 +0330

    Array & Objects

--- src/App.js
+++ src/App.js
@@ -8,16 +8,21 @@ function App() {
   const Job = "Web Dev";
   const inputPlaceHolder = "Enter your details"
   const input = <input placeholder={inputPlaceHolder}/>
+  const myArray = [0,1,2,3]
+  const myObject = {age : 35,name : "Fat Gum"}
 
   const getFullName = (firstName,lastName) => `${firstName} ${lastName}`;
   
   return (
     <div className="App">
       <h3>Full Name : {getFullName(firstName,lastName)}</h3>
-      <p>Job : {Job}</p>
-      <p>Age : {age}</p>
+      <p>Job : {myArray[1]}</p>
+      <p>Age : {myObject.age}</p>
       {/* <input placeholder={inputPlaceHolder}/> */}
       {input}
+      {
+        myArray[2] > 0  ? "True" : "False"
+      }
     </div>
   );
 }
