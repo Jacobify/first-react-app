e242e7c666a64ecc27ecea1b8cc04abd4a3ef580
Jacobify <farzadyaghoobi_1375@yahoo.com>
2022-11-12 14:30:09 +0330

    Jacobify - React css/Classes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
 // import logo from './logo.svg';
-import React from 'react';
 import './App.css';
 
 function App() {
-  // const firstName = "Farzad";
-  // const lastName = "Yaghoobi";
-  // const age = 25;
-  // const Job = "Web Dev";
-  // const inputPlaceHolder = "Enter your details"
-  // const input = <input placeholder={inputPlaceHolder}/>
-  // const myArray = [0,1,2,3]
-  // const myObject = {age : 35,name : "Fat Gum"}
-  // const getFullName = (firstName,lastName) => `${firstName} ${lastName}`;
-  
+  const firstName = "Farzad";
+  const lastName = "Yaghoobi";
+  const inputPlaceHolder = "Enter your details"
+  const myArray = [0,1,2,3]
+  const myObject = {age : 35,name : "Fat Gum"}
+  const getFullName = (firstName,lastName) => `${firstName} ${lastName}`;
+  const styles = {
+    margin : "16px",
+    borderRadius : "5px",
+    boxShadow: "5px 5px 5px #421"
+  }
   return (
-      
-/*#__PURE__*/React.createElement("div", {
-  className: "main-div"
-}, /*#__PURE__*/React.createElement("h3", null, "Heading"), /*#__PURE__*/React.createElement("p", null, "Para"), /*#__PURE__*/React.createElement("p", null, "Para"), /*#__PURE__*/React.createElement("p", null, "Para"), /*#__PURE__*/React.createElement("p", null, "Para"), /*#__PURE__*/React.createElement("p", null, "Para"), /*#__PURE__*/React.createElement("p", null, "Para"), /*#__PURE__*/React.createElement("p", null, "Para"))
+    <div className="App">
+      <div style={
+        {
+          margin : "16px",
+          borderRadius : "5px",
+          boxShadow: "5px 5px 5px #421"
+        }
+      }>
+        <h3>Full Name : {getFullName(firstName,lastName)}</h3>
+        <p>Job : {myArray[1]}</p>
+        <p>Age : {myObject.age}</p>
+        <input placeholder={inputPlaceHolder}/>
+        {
+          myArray[2] > 0  ? "True" : "False"
+        }
+        <h3>Blog Title 1</h3>
+        <p>Test para</p>
+      </div>
+      <div style={styles}>
+        <h3>Full Name : {getFullName(firstName,lastName)}</h3>
+        <p>Job : {myArray[1]}</p>
+        <p>Age : {myObject.age}</p>
+        <input placeholder={inputPlaceHolder}/>
+        {
+          myArray[2] > 0  ? "True" : "False"
+        }
+        <h3>Blog Title 1</h3>
+        <p>Test para</p>
+      </div>
+    
+      <div className="BlogCard"></div>
+        <div style={
+          {
+            margin : "16px",
+            borderRadius : "5px",
+            boxShadow: "5px 5px 5px #421"
+          }
+        }>
+          <h3>Full Name : {getFullName(firstName,lastName)}</h3>
+          <p>Job : {myArray[1]}</p>
+          <p>Age : {myObject.age}</p>
+          <input placeholder={inputPlaceHolder}/>
+          {
+            myArray[2] > 0  ? "True" : "False"
+          }
+          <h3>Blog Title 1</h3>
+          <p>Test para</p>
+        </div>
+        <div style={styles}>
+          <h3>Full Name : {getFullName(firstName,lastName)}</h3>
+          <p>Job : {myArray[1]}</p>
+          <p>Age : {myObject.age}</p>
+          <input placeholder={inputPlaceHolder}/>
+          {
+            myArray[2] > 0  ? "True" : "False"
+          }
+          <h3>Blog Title 1</h3>
+          <p>Test para</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
