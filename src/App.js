313b46a5b7765f5fa81c4f6cf61c244f90d58e09
Jacobify <farzadyaghoobi_1375@yahoo.com>
2023-02-21 13:36:34 +0330

    Jacobify - statefull & stateless

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 // import logo from './logo.svg';
 // import './App.css';
+import React, {Component} from 'react';
 import BlogCard from './BlogCard';
 import {isArrayEmpty as arrayValueCheck} from "./Utils"
 
-function App() {
+class App extends Component {
   // const firstName = "Farzad";
   // const lastName = "Yaghoobi";
   // const inputPlaceHolder = "Enter your details"
@@ -15,36 +16,55 @@ function App() {
   //   borderRadius : "5px",
   //   boxShadow: "5px 5px 5px #421"
   // }
-  const blogArr = [
-    {
-      title:"Blog Title 1",
-      description: "lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor"
-    },
-    {
-      title:"Blog Title 2",
-      description: "lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor"
-    },
-    {
-      title:"Blog Title 3",
-      description: "lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor"
-    }
-];  
-  const blogCards = arrayValueCheck(blogArr) ? [] : blogArr.map((blog,index)=>{
-    return(
-      <BlogCard key={index} title={blog.title} description={blog.description} id={index} />
-      // <div className="BlogCard" key={index}>
-      //   <h3>{blog.title}</h3>
-      //   <p>{blog.description}</p>
-      // </div>
-    )
-  
+
+  state = {
+    showBlogs : true
+  }
+
+  blogArr = [
+   {
+     title:"Blog Title 1",
+     description: "lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor"
+   },
+   {
+     title:"Blog Title 2",
+     description: "lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor"
+   },
+   {
+     title:"Blog Title 3",
+     description: "lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor"
+   }
+  ];
+  blogCards = arrayValueCheck(this.blogArr) ? [] : this.blogArr.map((blog,index)=>{
+   return(
+     <BlogCard key={index} title={blog.title} description={blog.description} id={index} />
+     // <div className="BlogCard" key={index}>
+     //   <h3>{blog.title}</h3>
+     //   <p>{blog.description}</p>
+     // </div>
+   )
   })
 
-  return (
-    <div className="App">
-      {blogCards}
-    </div>
-  );
+
+  onHideBtnClick = () => {
+    // this.showBlogs = false;
+    this.setState({showBlogs:false})
+    console.log('====================================');
+    console.log(this.state.showBlogs);
+    console.log('====================================');
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <button onClick={this.onHideBtnClick}>Hide List</button>
+        {
+          this.state.showBlogs ? this.blogCards : null
+        }
+      </div>
+    )
+  }
+
 }
 
 export default App;
