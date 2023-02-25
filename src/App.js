be16e093d1153b7d16b4750779cbf3d0eb8439fd
Jacobify <farzadyaghoobi_1375@yahoo.com>
2023-02-25 13:41:56 +0330

    Jacobify - passing functions as props

--- src/App.js
+++ src/App.js
@@ -34,55 +34,75 @@ class App extends Component {
     console.log(this.state.showBlogs);
     console.log('====================================');
   }
-  onLikeBtnClick = () => {
+  onLikeBtnClick = (pos) => {
     // this.setState((prevState,prevProp)=>{
     //     return {likeCount: prevState.likeCount + 1};
     // }
     // )
-    alert("like button is clicked!")
+    // alert("like button is clicked!")
+    // alert("like button is clicked at position : " + pos)
+    const updatedBlogList = this.state.blogArr;
+    const updatedBlogObj = updatedBlogList[pos];
+    // console.log("🚀 ~ file: App.js:46 ~ App ~ updatedBlogObj :", updatedBlogObj )
+
+    updatedBlogObj.likeCount += 1;
+    updatedBlogList[pos] = updatedBlogObj
+    this.setState({blogArr:updatedBlogList})
   }
   state = {
-    showBlogs : true
+    showBlogs : true,
+    blogArr : [
+      {
+        title:"Blog Title 1",
+        description: "lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor",
+        likeCount :0
+      },
+      {
+        title:"Blog Title 2",
+        description: "lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor",
+        likeCount :0
+      },
+      {
+        title:"Blog Title 3",
+        description: "lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor",
+        likeCount :0
+      }
+      ]
   }
 
-  blogArr = [
-   {
-     title:"Blog Title 1",
-     description: "lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor",
-     likeCount :0
-   },
-   {
-     title:"Blog Title 2",
-     description: "lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor",
-     likeCount :0
-   },
-   {
-     title:"Blog Title 3",
-     description: "lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor lorem impsum dolor",
-     likeCount :0
-   }
-  ];
 
-  blogCards = arrayValueCheck(this.blogArr) ? [] : this.blogArr.map((blog,index)=>{
-   return(
-     <BlogCard key={index} likeCount={blog.likeCount} title={blog.title} description={blog.description} id={index} onLikeBtnClick={this.onLikeBtnClick}/>
-     // <div className="BlogCard" key={index}>
-     //   <h3>{blog.title}</h3>
-     //   <p>{blog.description}</p>
-     // </div>
-   )
-  })
+  // blogCards = arrayValueCheck(this.state.blogArr) ? [] : this.state.blogArr.map((blog,index)=>{
+  // return(
+  //   <BlogCard key={index} likeCount={blog.likeCount} title={blog.title} description={blog.description} id={index} onLikeBtnClick={() => this.onLikeBtnClick(index)}/>
+  //    // <div className="BlogCard" key={index}>
+  //    //   <h3>{blog.title}</h3>
+  //    //   <p>{blog.description}</p>
+  //    // </div>
+  //   )
+  // })
 
 
 
 
   render() {
     console.log("render is called!");
+    console.log("this.state");
+    console.log(this.state);
+
+    const blogCards = arrayValueCheck(this.state.blogArr) ? [] : this.state.blogArr.map((blog,index)=>{
+      return(
+        <BlogCard key={index} likeCount={blog.likeCount} title={blog.title} description={blog.description} id={index} onLikeBtnClick={() => this.onLikeBtnClick(index)}/>
+         // <div className="BlogCard" key={index}>
+         //   <h3>{blog.title}</h3>
+         //   <p>{blog.description}</p>
+         // </div>
+        )
+      })
     return(
       <div className="App">
         <button onClick={this.onHideBtnClick}>{this.state.showBlogs ?"Hide List" : "Show List"}</button>
         {
-          this.state.showBlogs ? this.blogCards : null
+          this.state.showBlogs ? blogCards : null
         }
       </div>
     )
